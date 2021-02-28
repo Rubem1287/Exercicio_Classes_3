9ee0d03bb7b1042e25f66f4f037ff36ee1e0bb08
Rubem Silva <rubem.edu@outlook.com>
2021-02-28 15:40:23 -0300

    Add project files.

---
+++ Exercicio_Classes_3/Aluno.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio_Classes_3
+{
+    class Aluno
+    {
+        public string Nome;
+
+        public double Nota1;
+
+        public double Nota2;
+
+        public double Nota3;
+
+
+        public double Soma()
+        {
+            return (Nota1 + Nota2 + Nota3);
+        }
+
+        public double Falta()
+        {
+            double falta = 60 - (Nota1 + Nota2 + Nota3);
+
+            return (falta);
+        }
+
+    }
+}
---
+++ Exercicio_Classes_3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+namespace Exercicio_Classes_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.Write("Nome do aluno : ");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno : ");
+            aluno.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (aluno.Soma() >= 60.0)
+            {
+                Console.Write("Nota Final = " + aluno.Soma().ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine();
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.Write("Nota Final = " + aluno.Soma().ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine();
+                Console.WriteLine("REPROVADO");
+                Console.Write("Faltaram " + aluno.Falta().ToString("F2", CultureInfo.InvariantCulture) + " Pontos");
+            }
+        }
+    }
+}
